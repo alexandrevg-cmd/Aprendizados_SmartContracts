ebd2f453caa596f1e08cb5f2a5f11dc11feadd56
alexandrevg-cmd <60208317+alexandrevg-cmd@users.noreply.github.com>
2021-10-14 09:29:12 -0300

    Update ordem_dos_advs_do_brasil.sol
    Correções atualizadas

--- ordem_dos_advs_do_brasil.sol
+++ ordem_dos_advs_do_brasil.sol
@@ -3,20 +3,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract OrdemDosAdvogadosDoBrasilCarteira {
     
-    // *** Exercicio para categorizar os elementos da carteira da OAB no formato "smart-contracts" ***
+    // *** Exercicio para categorizar os elementos da carteira da OAB no formato "smart-contracts" ***    
+   
+    string constant public nomeDoInscritoOAB = "Alexandre Vichinheski Garcia";
+    string public filiacaoNomeDoPai = "David Garcia Neto";
+    string public filiacaoNomeDaMae = "Marisa Vichinheski Garcia";
+    string public naturalidadeCidade = "Porto Alegra";
+    string public naturalidadeEstado = "RS";
     
-    string public nomeDoInscritoOAB = "Alexandre Vichinheski Garcia";
-    string filiacaoNomeDoPai = "David Garcia Neto";
-    string filiacaoNomeDaMae = "Marisa Vichinheski Garcia";
-    string constant naturalidadeCidade = "Porto Alegra";
-    string constant naturalidadeEstado = "RS";
-    string constant rg = "3083069314";
-    string constant rgOrgaoEmissor = "SSP/RS";
-    string constant cpf = "97594291188";
-    uint constant public dataDeNascimento = 19801105;
-    uint public dataDeExpedicao = 20200219;
-    uint constant public inscricao = 123456789;
-    bool temCarteiraEtherium = true;
-    address carteiraEtherium =  0xEdadFE14fcB5aF91153C39d73cB7f66ea00f08E4;
+    // RG e CPF coloquei em 'string' pois previne o caso de documentos que começam com zero (0)...
+    string private rg = "3083069314";
+    string private rgOrgaoEmissor = "SSP/RS";
+    string private cpf = "97594291188";
+    uint public dataDeNascimento = 19801105;
+    uint public expedidoEm;
+    uint public inscricao = 123456789;
     
+    // O Boolean e o Address coloquei só para exercitar o uso...
+    bool public temCarteiraEtherium = true;
+    address constant public carteiraEtherium =  0xEdadFE14fcB5aF91153C39d73cB7f66ea00f08E4;
 }
